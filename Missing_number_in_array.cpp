afdb74a3364fa77073886ebcf754aa88bfb43327
Prateek Kanaujia <152027702+Prateek307@users.noreply.github.com>
2023-12-12 01:36:09 +0530

    Add files via upload

---
+++ Missing_number_in_array.cpp
@@ -0,0 +1,46 @@
+// link to the problem --> https://practice.geeksforgeeks.org/problems/missing-number-in-array1416/1?page=1&sprint=a663236c31453b969852f9ea22507634&sortBy=submissions
+
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+  public:
+    int missingNumber(vector<int>& array, int n) {
+        // Your code goes here
+        sort(array.begin(),array.end());
+        int i;
+        for(i=0;i<n;i++)
+        {
+            if(array[i]!=i+1)
+            return i+1;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+        Solution obj;
+        cout << obj.missingNumber(array, n) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
